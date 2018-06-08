f5d68cb88e8a138af5815b9481d95c1cc46525c4
Merel Hemstede <merel.hemstede@gmail.com>
2018-06-08 19:47:46 +0200

    Mijn eerste commit

---
+++ schaken.py
@@ -0,0 +1,49 @@
+#dit beschijft het bord van het schaakspel
+class Bord:
+	rijen = 8
+	colommen = 8
+
+	def __init__(self):
+		self.stukken = []
+
+	def __str__(self):
+		strBord = " ABCDEFGH\n"
+		for r in range( Bord.rijen): 
+			strBord += str( Bord.rijen - r)
+			for c in range( Bord.colommen ) : 
+				strBord += "-"
+			strBord += "\n"  	
+		return strBord
+
+
+class Kleur:
+	ZWART = 0
+	WIT =1
+
+
+class Stuk: 
+	def __init__(self, kleur):
+		self.kleur = kleur
+
+
+class Plek:
+	def __init__(self, colom, rij):
+		self.colom = colom
+		self.rij = rij
+
+	def __str__(self):
+		return chr( ord("A") + self.colom ) + str( self.rij + 1)
+
+
+
+
+
+#hierder wat test code
+b = Bord()
+pion = Stuk( Kleur.ZWART )
+
+p = Plek(3,5)
+
+print( b )
+
+  
